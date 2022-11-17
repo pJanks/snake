ecd725d7d0c453ed2dfbbbdb30ade94d19512ee9
pjanks <johnny.k.cassidy@gmail.com>
2022-11-17 13:14:52 -0800

    Initial commit

---
+++ backend/get_scores.php
@@ -0,0 +1,25 @@
+<?php
+try {
+    require_once '../config.php';
+    
+    $date = new DateTime();
+    $date = $date->format('m.d.y h:i:s A');
+
+    $sql = 'SELECT * FROM scores ORDER BY score DESC LIMIT 10';
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute();
+    
+    $scores = [];
+
+    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+      array_push($scores, $row);
+    }
+
+    $successMessage = "$date retrieved scores\n";
+    file_put_contents('../logs/log.log', $successMessage, FILE_APPEND);
+    echo json_encode($scores, JSON_PRETTY_PRINT);
+  } catch(PDOException $e) {
+    $errorMessage = $date . ' ERROR GETTING scores TABLE: ' . $e->getMessage() . ' line: ' . $e->getLine() . "\n";
+    file_put_contents('../logs/error.log', $errorMessage, FILE_APPEND);
+    echo '<h1 style="color: #F00; font-size: 240%; font-weight: bold;">ERR</h1>';
+  }
